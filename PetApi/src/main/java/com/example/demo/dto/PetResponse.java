813625cbd9eb4b09d0ce06c47d49aa123a2ff3ef
yukiyamadajp <hirameita.from.tokyo@gmail.com>
2025-07-08 16:33:09 +0900

    ストリーム処理のリファクタリングを実装

--- PetApi/src/main/java/com/example/demo/dto/PetResponse.java
+++ PetApi/src/main/java/com/example/demo/dto/PetResponse.java
@@ -5,9 +5,10 @@ import java.util.List;
 import com.example.demo.enums.PetStatus;
 
 import lombok.Data;
-
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class PetResponse {
     private int id;
     private Category category;
@@ -15,16 +16,16 @@ public class PetResponse {
     private List<String> photoUrls;
     private List<Tag> tags;
     private PetStatus status;
-    
-    
+
     @Data
+    @Accessors(chain = true)
     public static class Category {
         private int id;
         private String name;
     }
-    
-    
+
     @Data
+    @Accessors(chain = true)
     public static class Tag {
         private int id;
         private String name;
